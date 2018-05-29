bf61152a5e1a88289e4908e53cb954748f3acb71
Jeremy LUGAGNE <j.lugagne@tadaweb.com>
2018-05-29 15:19:49 +0200

    [#20] tune arguments for serve command

--- cmd/frontapi/serve.go
+++ cmd/frontapi/serve.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"os"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 
@@ -16,11 +18,21 @@ func init() {
 
 	serveCmd.PersistentFlags().Int("port", 8080, "Listenning port")
 
+	// Add 'dispatcher' flags
+	serveCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "../../configs/dispatcher.yaml", "Config file path")
+	serveCmd.PersistentFlags().StringP("loglevel", "l", "warn", "Log level (debug|info|warn|error)")
+	serveCmd.PersistentFlags().String("modulename", "", "Name of the module")
+	serveCmd.PersistentFlags().String("subscribestoevent", "", "Event this modules subscribes to")
+	serveCmd.PersistentFlags().String("eventspublished", "", "Events this modules can publish")
+	serveCmd.PersistentFlags().String("servicebusnamespace", "", "Namespace to use for ServiceBus")
+	serveCmd.PersistentFlags().String("resourcegroup", "", "Azure ResourceGroup to use")
+	serveCmd.PersistentFlags().Bool("logsensitiveconfig", false, "Print out sensitive config when logging")
+	serveCmd.PersistentFlags().String("moduleconfigpath", "", "Path to environment variables file for module")
+	serveCmd.PersistentFlags().BoolP("printconfig", "P", false, "Print out config when starting")
+	// job.*
+	serveCmd.PersistentFlags().Int("job.retrycount", 0, "Max number of times a job can be retried")
+
 	viper.BindPFlag("port", serveCmd.PersistentFlags().Lookup("port"))
-	viper.BindEnv("servicebus_namespace")
-	viper.BindEnv("servicebus_topic")
-	viper.BindEnv("servicebus_saspolicy")
-	viper.BindEnv("servicebus_accesskey")
 
 	viper.SetEnvPrefix("frontapi")
 	viper.AutomaticEnv()
@@ -29,13 +41,48 @@ func init() {
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Serve the HTTP handlers of frontapi",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		viper.SetConfigFile(cfgFile)
 		viper.SetConfigFile(cfgFile)
 		if err := viper.ReadInConfig(); err != nil {
 			log.WithError(err).Errorln("Can't read config")
 			os.Exit(1)
 		}
+		viper.AutomaticEnv()
+
+		// Fill config with global settings
+		cfg.LogLevel = viper.GetString("loglevel")
+		cfg.ModuleName = viper.GetString("modulename")
+		cfg.SubscribesToEvent = viper.GetString("subscribestoevent")
+		cfg.EventsPublished = viper.GetString("eventspublished")
+		cfg.ServiceBusNamespace = viper.GetString("servicebusnamespace")
+		cfg.ResourceGroup = viper.GetString("resourcegroup")
+		cfg.PrintConfig = viper.GetBool("printconfig")
+
+		// job.*
+		cfg.Job.RetryCount = viper.GetInt("job.retrycount")
+
+		// Globally set configuration level
+		switch strings.ToLower(cfg.LogLevel) {
+		case "debug":
+			log.SetLevel(log.DebugLevel)
+		case "info":
+			log.SetLevel(log.InfoLevel)
+		case "warn":
+			log.SetLevel(log.WarnLevel)
+		case "error":
+			log.SetLevel(log.ErrorLevel)
+		default:
+			log.SetLevel(log.WarnLevel)
+		}
+
+		hostName, err := os.Hostname()
+		if err != nil {
+			return errors.New("Unable to automatically set instanceid to hostname")
+		}
+		cfg.Hostname = hostName
+
+		return nil
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		frontapi.Run(&cfg, viper.GetInt("port"))
