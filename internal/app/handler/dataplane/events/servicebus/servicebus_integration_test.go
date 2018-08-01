3c7ef951c3ce832280ffbb3262078ae7260558bd
lawrencegripper <info@grippers.co.uk>
2018-08-01 17:36:05 +0100

    update

--- internal/app/handler/dataplane/events/servicebus/servicebus_integration_test.go
+++ internal/app/handler/dataplane/events/servicebus/servicebus_integration_test.go
@@ -72,8 +72,8 @@ func TestIntegration_serivcebusHTTPSender(t *testing.T) {
 	if msg.DeliveryAnnotations == nil || len(msg.DeliveryAnnotations) < 1 {
 		t.Errorf("expected delivery annotation to be set, have: %+v", msg)
 		t.Logf("msg properties: %+v", msg.Properties)
-		t.Log("msg annotation: %+v", msg.Annotations)
-		t.Log("msg delivery annotation: %+v", msg.DeliveryAnnotations)
+		t.Logf("msg annotation: %+v", msg.Annotations)
+		t.Logf("msg delivery annotation: %+v", msg.DeliveryAnnotations)
 	}
 
 	msg.Accept()
